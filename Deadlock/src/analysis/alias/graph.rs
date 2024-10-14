311b58c659283347fbc6272fe46749a5ac057d94
Andres-Nju <1572378310@qq.com>
2024-10-14 22:39:02 +0800

    raw pointer for alias analysis

--- Deadlock/src/analysis/alias/graph.rs
+++ Deadlock/src/analysis/alias/graph.rs
@@ -10,51 +10,268 @@ use super::node::{AliasGraphNode, GraphNodeId};
 
 
 pub struct AliasGraph{
-    nodes: FxHashSet<Rc<AliasGraphNode>>,
-    node_map: FxHashMap<GraphNodeId, Rc<AliasGraphNode>>,
+    nodes: FxHashSet<*mut AliasGraphNode>,
+    node_map: FxHashMap<*const AliasGraphNode, *mut AliasGraphNode>,
+}
+
+impl Drop for AliasGraph{
+    fn drop(&mut self) {  
+        for &node_ptr in self.nodes.iter() {
+            unsafe {
+                drop(Box::from_raw(node_ptr));
+            }
+        }
+    }
 }
 
 impl AliasGraph{
-    pub fn new() -> Self{
-        AliasGraph{
+    pub fn new() -> Self {
+        AliasGraph {
             nodes: FxHashSet::default(),
             node_map: FxHashMap::default(),
         }
-    } 
-
-    pub fn get_or_insert_node(&mut self, node_id: GraphNodeId) -> Rc<AliasGraphNode>{
-        // if cannot find the node, create one and insert it to the graph
-        self.node_map.entry(node_id.clone()).or_insert(AliasGraphNode::new(node_id, None)).clone()
     }
 
-    pub fn combine(&mut self, node_x: AliasGraphNode, node_y: AliasGraphNode){
-        
+    pub fn find_vertex(&self, val: *mut AliasGraphNode) -> Option<*mut AliasGraphNode> {
+        self.node_map.get(&(val as *const _)).copied()
     }
 
-    pub fn resolve_project(&mut self, def_id: &DefId, p: &Place) -> Rc<AliasGraphNode> {
-        let mut cur_node_id = GraphNodeId::new(def_id.clone(), p.local.as_usize());
-        let mut cur_node = self.get_or_insert_node(cur_node_id);
-        println!("{:?}: {:?}", p.local, p.projection);
-        for projection in p.projection{
-            match &projection{ // TODO: complex types
-                mir::ProjectionElem::Deref => {
-                    let deref_set = cur_node.get_predecessors_by_label(&EdgeLabel::Deref);
-                    if let Some(deref_set) = deref_set{
-                        
-                    }
-                    else{
-                        panic!("There is no deref target for {:?}", cur_node);
-                    }
-                },
-                mir::ProjectionElem::Field(_, _) => (),
-                mir::ProjectionElem::Index(_) => todo!(),
-                mir::ProjectionElem::ConstantIndex { .. } => todo!(),
-                mir::ProjectionElem::Subslice { .. } => todo!(),
-                mir::ProjectionElem::Downcast(_, _) => todo!(),
-                mir::ProjectionElem::OpaqueCast(_) => todo!(),
-                mir::ProjectionElem::Subtype(_) => todo!(),
+    pub fn add_node(&mut self, id: GraphNodeId, name: Option<String>) -> *mut AliasGraphNode {
+        let node_ptr = AliasGraphNode::new(id, name);
+        self.nodes.insert(node_ptr);
+        self.node_map.insert(node_ptr as *const AliasGraphNode, node_ptr);
+        node_ptr
+    }
+
+    // Combine two nodes into one, merging NodeY into NodeX
+    pub fn combine(&mut self, node_x: *mut AliasGraphNode, node_y: *mut AliasGraphNode) -> *mut AliasGraphNode {
+        unsafe {
+            assert!(self.nodes.contains(&node_x));
+            assert!(self.nodes.contains(&node_y));
+
+            if node_x == node_y {
+                return node_x;
             }
+
+            // Ensure node_x has the higher degree
+            if (*node_x).degree() < (*node_y).degree() {
+                std::mem::swap(&mut node_x, &mut node_y);
+            }
+
+            // Merge NodeY's outgoing labels and targets into NodeX
+            let y_out_labels = &*(*node_y).out_labels;
+            for label in y_out_labels.iter() {
+                if (*node_y).contains_target(node_y, *label) {
+                    if !(*node_x).contains_target(node_x, *label) {
+                        (*node_x).add_target(node_x, *label);
+                    }
+                    (*node_y).remove_target(node_y, *label);
+                }
+            }
+
+            // Move NodeY's outgoing vertices to NodeX
+            for label in y_out_labels.iter() {
+                if let Some(out_vertices) = (*(*node_y).successors).get(label) {
+                    for w in (**out_vertices).iter() {
+                        if !(*node_x).contains_target(*w, *label) {
+                            (*node_x).add_target(*w, *label);
+                        }
+                        // Modify safely without affecting the iterator
+                        let w_temp = *w;
+                        (*(*node_y).successors).get_mut(label).unwrap().remove(&w_temp);
+                        (*w_temp).get_in_vertices(*label).unwrap().remove(&node_y);
+                    }
+                }
+            }
+
+            // Merge NodeY's incoming vertices into NodeX
+            let y_in_labels = &*(*node_y).in_labels;
+            for label in y_in_labels.iter() {
+                if let Some(in_vertices) = (*(*node_y).predecessors).get(label) {
+                    for w in in_vertices.iter() {
+                        if !(*w).contains_target(node_x, *label) {
+                            (*w).add_target(node_x, *label);
+                        }
+                        let w_temp = *w;
+                        (*(*node_y).predecessors).get_mut(label).unwrap().remove(&w_temp);
+                        (*w_temp).get_out_vertices(*label).unwrap().remove(&node_y);
+                    }
+                }
+            }
+
+            // Move equivalence class of NodeY to NodeX
+            let vals = (*node_y).get_alias_set();
+            for &val in vals.iter() {
+                self.node_map.insert(val as *const _, node_x);
+            }
+            (*node_y).mv_alias_set_to(node_x);
+
+            // Remove NodeY from the graph and delete it
+            self.nodes.remove(&node_y);
+            drop(Box::from_raw(node_y)); // Safe deletion of NodeY
+
+            node_x
         }
-        cur_node
+    }
+
+    // pub fn resolve_project(&mut self, def_id: &DefId, p: &Place) -> Rc<AliasGraphNode> {
+    //     let mut cur_node_id = GraphNodeId::new(def_id.clone(), p.local.as_usize());
+    //     let mut cur_node = self.get_or_insert_node(cur_node_id);
+    //     println!("{:?}: {:?}", p.local, p.projection);
+    //     for projection in p.projection{
+    //         match &projection{ // TODO: complex types
+    //             mir::ProjectionElem::Deref => {
+    //                 let deref_set = cur_node.get_predecessors_by_label(&EdgeLabel::Deref);
+    //                 if let Some(deref_set) = deref_set{
+                        
+    //                 }
+    //                 else{
+    //                     panic!("There is no deref target for {:?}", cur_node);
+    //                 }
+    //             },
+    //             mir::ProjectionElem::Field(_, _) => (),
+    //             mir::ProjectionElem::Index(_) => todo!(),
+    //             mir::ProjectionElem::ConstantIndex { .. } => todo!(),
+    //             mir::ProjectionElem::Subslice { .. } => todo!(),
+    //             mir::ProjectionElem::Downcast(_, _) => todo!(),
+    //             mir::ProjectionElem::OpaqueCast(_) => todo!(),
+    //             mir::ProjectionElem::Subtype(_) => todo!(),
+    //         }
+    //     }
+    //     cur_node
+    // }
+
+    pub fn print(&self){
+        for &node_ptr in &self.nodes{
+            unsafe{
+                (*node_ptr).print_node();
+            }
+            println!();
+        }
+    }
+    pub fn print_graph(&self) {
+        for &node_ptr in &self.nodes {
+            unsafe {
+                let node = &*node_ptr;
+                println!("Node ID: {:?}", node.id);
+                println!("  Name: {:?}", node.name);
+                    
+                // print alias_set
+                println!("  Alias Set:");
+                if !node.alias_set.is_null() {
+                    let alias_set = &*node.alias_set;
+                    for &alias in alias_set.iter() {
+                        let alias_node = &*alias;
+                        println!("    - Node ID: {:?}", alias_node.id);
+                    }
+                }
+
+                // print out_labels
+                println!("  Out Labels:");
+                if !node.out_labels.is_null() {
+                    let out_labels_set = &*node.out_labels;
+                    for &label in out_labels_set.iter() {
+                        println!("    - {:?}", *label); 
+                    }
+                }
+
+                // print in_labels
+                println!("  In Labels:");
+                if !node.in_labels.is_null() {
+                    let in_labels_set = &*node.in_labels; 
+                    for &label in in_labels_set.iter() {
+                        println!("    - {:?}", *label); 
+                    }
+                }
+
+                // print successors
+                println!("  Successors:");
+                if !node.successors.is_null() {
+                    let successors_map = &*node.successors; 
+                    for (label, successors_set) in successors_map.iter() {
+                        println!("    - Label: {:?}", **label);
+                        for &successor in &**successors_set {
+                            let successor_node = &*successor; 
+                            println!("      - Node ID: {:?}", successor_node.id);
+                        }
+                    }
+                }
+
+                // print predecessors
+                println!("  Predecessors:");
+                if !node.predecessors.is_null() {
+                    let predecessors_map = &*node.predecessors; 
+                    for (label, predecessors_set) in predecessors_map.iter() {
+                        println!("    - Label: {:?}", **label);
+                        for &predecessor in &**predecessors_set {
+                            let predecessor_node = &*predecessor; 
+                            println!("      - Node ID: {:?}", predecessor_node.id);
+                        }
+                    }
+                }
+            }
+            println!();
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests{
+    use rustc_hir::def_id::DefIndex;
+
+    use super::*;
+    #[test]
+    fn test_add_remove_target(){
+        let mut graph = AliasGraph::new();
+    
+        // add 2 nodes
+        let node1 = graph.add_node(GraphNodeId::new(DefId::local(DefIndex::from_u32(1)), 20), Some("Node 1".to_string()));
+        let node2 = graph.add_node(GraphNodeId::new(DefId::local(DefIndex::from_u32(2)), 21), Some("Node 2".to_string()));
+        
+        unsafe {
+            let label1 = Box::into_raw(Box::new(EdgeLabel::Deref));
+            (*node1).add_target(node2, label1);
+
+            // print
+            println!("test1 ");
+            graph.print_graph();
+
+            (*node1).remove_target(node2, label1);
+            // print
+            println!("test2 ");
+            graph.print_graph();
+
+            let label2 = Box::into_raw(Box::new(EdgeLabel::Guard));
+            (*node2).add_target(node1, label2);
+            // print
+            println!("test3 ");
+            graph.print_graph();
+
+            graph.print();
+        }
+    }
+
+    #[test]
+    fn test_mv_alias_set(){
+        let mut graph = AliasGraph::new();
+    
+        // add 2 nodes
+        let node1 = graph.add_node(GraphNodeId::new(DefId::local(DefIndex::from_u32(1)), 20), Some("Node 1".to_string()));
+        let node2 = graph.add_node(GraphNodeId::new(DefId::local(DefIndex::from_u32(2)), 21), Some("Node 2".to_string()));
+        
+        unsafe {
+            // move node1's set into node2
+            let label1 = Box::into_raw(Box::new(EdgeLabel::Deref));
+            (*node1).add_target(node2, label1);
+            (*node1).mv_alias_set_to(node2);
+            // print
+            println!("test1 ");
+            graph.print_graph();
+
+            // move node2's set into node1
+            (*node2).mv_alias_set_to(node1);
+            println!("test2 ");
+            graph.print_graph();
+        }
     }
 }
--- Deadlock/src/analysis/alias/node.rs
+++ Deadlock/src/analysis/alias/node.rs
@@ -5,18 +5,18 @@ use rustc_hir::def_id::DefId;
 
 
 pub struct AliasGraphNode{
-    id: GraphNodeId,
-    name: Option<String>,
+    pub id: GraphNodeId,
+    pub name: Option<String>,
 
-    alias_set: FxHashSet<Rc<AliasGraphNode>>,
+    pub alias_set: *mut FxHashSet<*mut AliasGraphNode>,
     
-    out_labels: FxHashSet<Rc<EdgeLabel>>,
-    in_labels: FxHashSet<Rc<EdgeLabel>>,
+    pub out_labels: *mut FxHashSet<*mut EdgeLabel>,
+    pub in_labels: *mut FxHashSet<*mut EdgeLabel>,
 
     /// target nodes pointed by this node
-    successors: FxHashMap<EdgeLabel, RefCell<FxHashSet<Rc<AliasGraphNode>>>>,
+    pub successors: *mut FxHashMap<*mut EdgeLabel, *mut FxHashSet<*mut AliasGraphNode>>,
     /// source nodes pointing to this node
-    predecessors: FxHashMap<EdgeLabel, RefCell<FxHashSet<Rc<AliasGraphNode>>>>,
+    pub predecessors: *mut FxHashMap<*mut EdgeLabel, *mut FxHashSet<*mut AliasGraphNode>>,
 }
 
 impl Debug for AliasGraphNode{
@@ -25,55 +25,258 @@ impl Debug for AliasGraphNode{
     }
 }
 impl AliasGraphNode{
-    pub fn new(id: GraphNodeId, name: Option<String>) -> Rc<Self>{
-        Rc::new(AliasGraphNode{
+    pub fn new(id: GraphNodeId, name: Option<String>) -> *mut AliasGraphNode {
+        let alias_set = Box::new(FxHashSet::default());
+        let out_labels = Box::new(FxHashSet::default());
+        let in_labels = Box::new(FxHashSet::default());
+        let successors = Box::new(FxHashMap::default());
+        let predecessors = Box::new(FxHashMap::default());
+
+        let node = Box::into_raw(Box::new(AliasGraphNode {
             id,
             name,
-            alias_set: FxHashSet::default(),
-            in_labels: FxHashSet::default(),
-            out_labels: FxHashSet::default(),
-            successors: FxHashMap::default(),
-            predecessors: FxHashMap::default(),
-        })
+            alias_set: Box::into_raw(alias_set),
+            out_labels: Box::into_raw(out_labels),
+            in_labels: Box::into_raw(in_labels),
+            successors: Box::into_raw(successors),
+            predecessors: Box::into_raw(predecessors),
+        }));
+        // add self into the alias set
+        unsafe {
+            (*(*node).alias_set).insert(node);
+        }
+        node
     }
 
-    pub fn get_alias_set(&self) -> &FxHashSet<Rc<AliasGraphNode>> {
-        &self.alias_set
+    // Get the node's name
+    pub fn get_name(&self) -> &Option<String> {
+        &self.name
     }
 
-    pub fn get_alias_set_mut(&mut self) -> &mut FxHashSet<Rc<AliasGraphNode>> {
-        &mut self.alias_set
+    // Get the number of outgoing vertices for a given label
+    pub fn out_num_vertices(&self, label: *mut EdgeLabel) -> usize {
+        unsafe {
+            let out_map = &*self.successors; // Dereference to get the map
+            if let Some(vertices) = out_map.get(&label) {
+                (**vertices).len()
+            } else {
+                0
+            }
+        }
     }
 
-    // pub fn get_successors(&self) -> Ref<FxHashMap<EdgeLabel, RefCell<FxHashSet<Rc<AliasGraphNode>>>>> {
-    //     self.successors.borrow()
-    // }
-
-    // pub fn get_successors_mut(&self) -> RefMut<FxHashMap<EdgeLabel, RefCell<FxHashSet<Rc<AliasGraphNode>>>>> {
-    //     self.successors.borrow_mut()
-    // }
-
-    // pub fn get_predecessors(&self) -> Ref<FxHashMap<EdgeLabel, RefCell<FxHashSet<Rc<AliasGraphNode>>>>> {
-    //     self.predecessors.borrow()
-    // }
-
-    // pub fn get_predecessors_mut(&self) -> RefMut<FxHashMap<EdgeLabel, RefCell<FxHashSet<Rc<AliasGraphNode>>>>> {
-    //     self.predecessors.borrow_mut()
-    // }
-
-    pub fn get_successors_by_label(&self, label: &EdgeLabel) -> Option<FxHashSet<Rc<AliasGraphNode>>> {
-        self.successors.get(label).map(|set_refcell| {
-            set_refcell.borrow().clone()
-        })
+    // Get the number of incoming vertices for a given label
+    pub fn in_num_vertices(&self, label: *mut EdgeLabel) -> usize {
+        unsafe {
+            let in_map = &*self.predecessors; // Dereference to get the map
+            if let Some(vertices) = in_map.get(&label) {
+                (**vertices).len()
+            } else {
+                0
+            }
+        }
     }
 
-    pub fn get_predecessors_by_label(&self, label: &EdgeLabel) -> Option<FxHashSet<Rc<AliasGraphNode>>> {
-        self.predecessors.get(label).map(|set_refcell| {
-            set_refcell.borrow().clone()
-        })
+    // Get the degree of the node
+    pub fn degree(&self) -> usize {
+        unsafe {
+            let mut ret = 0;
+
+            // Calculate incoming vertices count
+            for vertices in (*self.predecessors).values() {
+                ret += (**vertices).len();
+            }
+
+            // Calculate outgoing vertices count
+            for vertices in (*self.successors).values() {
+                ret += (**vertices).len();
+            }
+
+            ret
+        }
     }
 
+    // Add a target node with a label
+    pub fn add_target(&mut self, node: *mut AliasGraphNode, label: *mut EdgeLabel) {
+        unsafe {
+            let out_map = &mut *self.successors; 
+            let out_labels_set = &mut *self.out_labels;
+
+            out_labels_set.insert(label);
+            let out_nodes = &mut **out_map.entry(label).or_insert_with(|| Box::into_raw(Box::new(FxHashSet::default())));
+            out_nodes.insert(node);
+            (*node).add_source(self as *mut _, label);
+        }
+    }
+
+    // Remove a target node with a label
+    pub fn remove_target(&mut self, node: *mut AliasGraphNode, label: *mut EdgeLabel) {
+        unsafe {
+            let out_map = &mut *self.successors; // Dereference the pointer
+
+            if let Some(set) = out_map.get_mut(&label) {
+                (&mut **set).remove(&node);
+            }
+            (*node).remove_source(self as *mut _, label); // Use unsafe to remove source
+        }
+    }
+
+    // Check if the node contains a specific target node
+    pub fn contains_target(&self, target: *mut AliasGraphNode, label: *mut EdgeLabel) -> bool {
+        unsafe {
+            (*self.successors).get(&label).map_or(false, |set| (&mut **set).contains(&target))
+        }
+    }
+
+    // Get the set of incoming vertices for a given label
+    pub fn get_in_vertices(&self, label: *mut EdgeLabel) -> Option<*mut FxHashSet<*mut AliasGraphNode>> {
+        unsafe {
+            (*self.predecessors).get(&label).copied()
+        }
+    }
+
+    // Get the set of outgoing vertices for a given label
+    pub fn get_out_vertices(&self, label: *mut EdgeLabel) -> Option<*mut FxHashSet<*mut AliasGraphNode>> {
+        unsafe {
+            (*self.successors).get(&label).copied()
+        }
+    }
+
+    // Get a unique incoming vertex for a label, if exists
+    pub fn get_in_vertex(&self, label: *mut EdgeLabel) -> Option<*mut AliasGraphNode> {
+        unsafe {
+            let set = self.get_in_vertices(label)?;
+            if (*set).len() == 1 {
+                (*set).iter().next().copied() // Copy the raw pointer
+            } else {
+                None
+            }
+        }
+    }
+
+    // Get a unique outgoing vertex for a label, if exists
+    pub fn get_out_vertex(&self, label: *mut EdgeLabel) -> Option<*mut AliasGraphNode> {
+        unsafe {
+            let set = self.get_out_vertices(label)?;
+            if (*set).len() == 1 {
+                (*set).iter().next().copied() // Copy the raw pointer
+            } else {
+                None
+            }
+        }
+    }
+
+     // Returns a reference to the alias set
+     pub fn get_alias_set(&self) -> *mut FxHashSet<*mut AliasGraphNode> {
+        unsafe {
+            self.alias_set
+        }
+    }
+
+    // Moves the alias set of the current node to the RootRep node
+    pub fn mv_alias_set_to(&mut self, root_rep: *mut AliasGraphNode) {
+        // If the root representative is the current node, no action is needed
+        if root_rep == self as *mut _ {
+            return;
+        }
+
+        unsafe {
+            // Get alias sets for both the current node and the root representative
+            let root_alias_set = &mut *(*root_rep).alias_set; // Dereference to get the alias set of root_rep
+            let this_alias_set = &mut *self.alias_set; // Dereference to get the alias set of self
+
+            // Insert all elements from the current node's alias set into the root_rep's alias set
+            root_alias_set.extend(this_alias_set.iter().copied());
+        }
+    }
+
+    // Add a source node with a label (private function)
+    fn add_source(&mut self, node: *mut AliasGraphNode, label: *mut EdgeLabel) {
+        unsafe {
+            let in_map = &mut *self.predecessors; // Dereference the pointer
+            (*self.in_labels).insert(label);
+            let in_nodes = in_map.entry(label).or_insert_with(|| Box::into_raw(Box::new(FxHashSet::default())));
+            (&mut **in_nodes).insert(node);
+        }
+    }
+
+    // Remove a source node with a label (private function)
+    fn remove_source(&mut self, node: *mut AliasGraphNode, label: *mut EdgeLabel) {
+        unsafe {
+            if let Some(set) = (*self.predecessors).get_mut(&label) {
+                (**set).remove(&node);
+            }
+        }
+    }
 }
+
+impl Drop for AliasGraphNode {
+    fn drop(&mut self) {
+        unsafe {
+            if !self.out_labels.is_null() {
+                let out_labels_set = Box::from_raw(self.out_labels);
+                for &label_ptr in out_labels_set.iter() {
+                    drop(Box::from_raw(label_ptr)); 
+                }
+            }
+            
+            if !self.in_labels.is_null() {
+                let in_labels_set = Box::from_raw(self.in_labels);
+                for &label_ptr in in_labels_set.iter() {
+                    drop(Box::from_raw(label_ptr)); 
+                }
+            }
+            
+            drop(Box::from_raw(self.out_labels));
+            drop(Box::from_raw(self.in_labels)); 
+        }
+    }
+}
+
+impl AliasGraphNode {
+    pub fn print_node(&self) {
+        unsafe {
+            // print current node id
+            println!("node id: {:?}", self.id);
+
+            // print alias set
+            println!("  alias set:");
+            let alias_set = &*self.alias_set;
+            for node in alias_set.iter(){
+                println!("      {:?}", **node);
+            }
+
+            // Print outgoing edges in the format A --label--> B
+            println!("  outgoing edges:");
+            let out_map = &*self.successors; // Dereference the pointer to get the map
+            for (label, successors) in out_map.iter() {
+                for &successor in (**successors).iter() {
+                    let successor_node = &*successor; // Dereference successor node
+                    let label_str = &**label; // Dereference to get the label value
+                    let node_id = &self.id;
+                    let succ_id = &successor_node.id;
+                    println!("      self --{:?}--> {:?}", label_str, succ_id);
+                }
+            }
+
+            println!();
+
+            // Print incoming edges in the format A --label--> B
+            println!("  incoming edges:");
+            let in_map = &*self.predecessors;
+            for (label, predecessors) in in_map.iter() {
+                for &predecessor in (**predecessors).iter() {
+                    let predecessor_node = &*predecessor; // Dereference successor node
+                    let label_str = &**label; // Dereference to get the label value
+                    let node_id = &self.id;
+                    let pre_id = &predecessor_node.id;
+                    println!("      {:?} --{:?}--> self", pre_id, label_str);
+                }
+            }
+        }
+    }
+}
+
 
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 pub struct GraphNodeId{
@@ -95,4 +298,14 @@ pub enum EdgeLabel{
     Deref,
     Guard,
     // todo: field, array access
+}
+
+impl From<&str> for EdgeLabel {
+    fn from(label: &str) -> Self {
+        match label {
+            "Deref" => EdgeLabel::Deref,
+            "Guard" => EdgeLabel::Guard,
+            _ => panic!("Unknown edge label!"),
+        }
+    }
 }
