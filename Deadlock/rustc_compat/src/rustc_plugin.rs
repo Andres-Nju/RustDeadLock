64efdefbae51191a3ebecfc6d711c146df953f24
Andres-Nju <1572378310@qq.com>
2024-11-12 23:33:07 +0800

    update

--- Deadlock/rustc_compat/src/rustc_plugin.rs
+++ Deadlock/rustc_compat/src/rustc_plugin.rs
@@ -9,9 +9,9 @@ use super::plugin::{Plugin, PLUGIN_ARGS};
 use crate::cargo_plugin::{RUN_ON_ALL_CRATES, SPECIFIC_CRATE, SPECIFIC_TARGET};
 use rustc_session::{config::ErrorOutputType, EarlyDiagCtxt};
 
-/// 来自 clippy
-/// 如果命令行选项与 `arg_to_be_found` 匹配，则对其值应用谓词 `pred`。如果为真，则返回该值，否则返回None。
-/// 参数假定为 `--arg=value` 或 `--arg value` 格式。
+/// If a command-line option matches `find_arg`, then apply the predicate `pred` on its value. If
+/// true, then return it.
+/// The parameter is assumed to be either `--arg=value` or `--arg value`.
 fn arg_value<'a, T: Deref<Target = str>>(
     args: &'a [T],
     find_arg: &str,
@@ -43,24 +43,29 @@ fn toolchain_path(home: Option<String>, toolchain: Option<String>) -> Option<Pat
     })
 }
 
-// 获取系统根目录，按照下面的顺序
-// - 命令行
-// - 运行时环境变量
+// Get the sysroot, looking from most specific to this invocation to the least:
+// - command line
+// - runtime environment
 //    - SYSROOT
-//    - RUSTUP_HOME, RUSTUP_TOOLCHAIN
-// - rustc打印的sysroot
-// - 编译时环境变量
+//    - RUSTUP_HOME, MULTIRUST_HOME, RUSTUP_TOOLCHAIN, MULTIRUST_TOOLCHAIN
+// - sysroot from rustc in the path
+// - compile-time environment
 //    - SYSROOT
-//    - RUSTUP_HOME, RUSTUP_TOOLCHAIN
+//    - RUSTUP_HOME, MULTIRUST_HOME, RUSTUP_TOOLCHAIN, MULTIRUST_TOOLCHAIN
 fn get_sysroot(orig_args: &[String]) -> (bool, String) {
     let sys_root_arg = arg_value(orig_args, "--sysroot", |_| true);
     let have_sys_root_arg = sys_root_arg.is_some();
     let sys_root = sys_root_arg
         .map(PathBuf::from)
+        .or_else(|| std::env::var("MIRI_SYSROOT").ok().map(PathBuf::from))
         .or_else(|| std::env::var("SYSROOT").ok().map(PathBuf::from))
         .or_else(|| {
-            let home = std::env::var("RUSTUP_HOME").ok();
-            let toolchain = std::env::var("RUSTUP_TOOLCHAIN").ok();
+            let home = std::env::var("RUSTUP_HOME")
+                .or_else(|_| std::env::var("MULTIRUST_HOME"))
+                .ok();
+            let toolchain = std::env::var("RUSTUP_TOOLCHAIN")
+                .or_else(|_| std::env::var("MULTIRUST_TOOLCHAIN"))
+                .ok();
             toolchain_path(home, toolchain)
         })
         .or_else(|| {
@@ -74,8 +79,12 @@ fn get_sysroot(orig_args: &[String]) -> (bool, String) {
         })
         .or_else(|| option_env!("SYSROOT").map(PathBuf::from))
         .or_else(|| {
-            let home = option_env!("RUSTUP_HOME").map(ToString::to_string);
-            let toolchain = option_env!("RUSTUP_TOOLCHAIN").map(ToString::to_string);
+            let home = option_env!("RUSTUP_HOME")
+                .or(option_env!("MULTIRUST_HOME"))
+                .map(ToString::to_string);
+            let toolchain = option_env!("RUSTUP_TOOLCHAIN")
+                .or(option_env!("MULTIRUST_TOOLCHAIN"))
+                .map(ToString::to_string);
             toolchain_path(home, toolchain)
         })
         .map(|pb| pb.to_string_lossy().to_string())
@@ -99,31 +108,32 @@ pub fn rustc_main<T: Plugin>(plugin: T) {
 
         let (have_sys_root_arg, sys_root) = get_sysroot(&orig_args);
 
+        println!("origin args: {:?}", &orig_args);
         if orig_args.iter().any(|a| a == "--version" || a == "-V") {
             let version_info = rustc_tools_util::get_version_info!();
             println!("{version_info}");
             exit(0);
         }
 
-        // 设置 RUSTC_WRAPPER 会导致 Cargo 传递 'rustc' 作为第一个参数。
-        // 我们自动调用编译器，因此忽略这个参数。
-        // 检查是否在 RUSTC_WRAPPER 模式下
-
+        // Setting RUSTC_WRAPPER causes Cargo to pass 'rustc' as the first argument.
+        // We're invoking the compiler programmatically, so we ignore this
+        // check whether in mode RUSTC_WRAPPER
         if orig_args.get(1).map(Path::new).and_then(Path::file_stem) == Some("rustc".as_ref()) {
             // we still want to be able to invoke it normally though
             orig_args.remove(1);
         }
 
-        // 此条件检查 --sysroot 标志，以便用户可以直接调用驱动程序而无需传递 --sysroot 或其他参数。
+        // this conditional check for the --sysroot flag is there so users can call
+        // the driver directly without having to pass --sysroot or anything
         let mut args: Vec<String> = orig_args.clone();
         if !have_sys_root_arg {
             args.extend(["--sysroot".into(), sys_root]);
         };
 
-        // 在一次 rustc 调用中，我们必须决定是作为 rustc 运行，还是实际执行插件。
-        // 执行插件有两个条件：
-        // 1. 要么我们应该运行所有 crate，或者设置了 CARGO_PRIMARY_PACKAGE。
-        // 2. 没有传递 --print，因为 Cargo 会这样做以获取 rustc 的信息。
+        // On a given invocation of rustc, we have to decide whether to act as rustc,
+        // or actually execute the plugin. There are two conditions for executing the plugin:
+        // 1. Either we're supposed to run on all crates, or CARGO_PRIMARY_PACKAGE is set.
+        // 2. --print is NOT passed, since Cargo does that to get info about rustc.
         let primary_package = env::var("CARGO_PRIMARY_PACKAGE").is_ok();
         let run_on_all_crates = env::var(RUN_ON_ALL_CRATES).is_ok();
         let normal_rustc = arg_value(&args, "--print", |_| true).is_some();
